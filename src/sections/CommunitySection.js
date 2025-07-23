eec7a13d0d5b7a1a8c8f845d62f1213ed55cea5a
blazhen <55562862+blazhen@users.noreply.github.com>
2025-07-23 14:21:11 +0200

    community heading width changed

--- src/sections/CommunitySection.js
+++ src/sections/CommunitySection.js
@@ -2,7 +2,7 @@ export default function CommunitySection() {
   return (
     <section className="bg-[#D2F944] py-6 px-4 ">
       <div className="mx-auto rounded-xl border border-[#747474] md:px-32 md:py-60 px-8 py-16">
-        <h2 className="text-[42px] md:text-[72px] font-normal text-[#252525] text-left leading-tight">
+        <h2 className="text-[42px] md:text-[72px] font-normal text-[#252525] text-left leading-tight max-w-6xl">
           JOIN OUR COMMUNITY &amp; SHARE YOUR EXPERIENCE
         </h2>
       </div>
